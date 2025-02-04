0fafd694d593ddcc3bfcfba465470dcb488e8e4d
Tomas Bruha <tomasbruha1@gmail.com>
2025-02-04 11:56:07 +0100

    checks done

--- fractal.c
+++ fractal.c
@@ -6,109 +6,68 @@
 /*   By: tbruha <tbruha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:21:27 by tbruha            #+#    #+#             */
-/*   Updated: 2025/02/03 17:09:02 by tbruha           ###   ########.fr       */
+/*   Updated: 2025/02/04 11:46:59 by tbruha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// DO NOW: Put image onto window
-// put pixels (Pollock) onto my window
-// key hook to print pressed key
-// Mandelbrot video by Oceano
+// DO NOW: Fractal (Mandelbrot part) video by Oceano
+
+// Check if I can use ft_printf for double.
+//
 
 // FUNCTIONs
 // mlx_init() - initializes the connection to MLX.
 // mlx_new_image() - creates a new blank image. // buffer probably
 // mlx_image_to_window() - It will put buffered image to a window.
-// mlx_loop_hook() - 
+// mlx_loop_hook() - It will keep "open" function in an infinite loop.
+// mlx_is_key_down() - Do something if specific keyboard is pressed.
+// mlx_close_window() - It tells mlx to close the open window.
+// mlx_terminate() - Terminates MLX and cleans up any left used resourcel.
 // mlx_loop() - It will keep the window openn, infinite loop.
 // mlx_terminate() - It will terminate the mlx connection
 
 #include "include/fractal.h"
 
-#define WIDTH 1024
-#define HEIGHT 718
-
-static mlx_image_t *image;
-
-void	ft_hook(void *param)
+int	main(int argc, char **argv)
 {
-	mlx_t *mlx = param;
-
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-		image->instances[0].y -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-		image->instances[0].y += 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-		image->instances[0].x -= 5;
-	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-		image->instances[0].x += 5;
-}
-
-uint32_t	ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-{
-	return (r << 24 | g << 16 | b << 8 | a);
-}
-
-void	ft_randomize(void *param)
-{
-	uint32_t	i;
-	uint32_t	y;
-	uint32_t	color;
+	t_complex	z;
+	t_complex	c;
+	double 		tmp_real;
 	
-	i = 0;
-	color = 0;
-	(void)param;
-	while (i < image->width)
+	// Checks for correct arguments
+	if (!((argc == 2 && ft_strncmp(argv[1], "Mandelbrot", 11) == 0)
+	|| (argc == 4 && ft_strncmp(argv[1], "Julia", 6) == 0)))
 	{
-		y = 0;
-		while (y < image->height)
-		{
-			color = ft_pixel(
-				rand() % 0xFF,
-				rand() % 0xFF,
-				rand() % 0xFF,
-				rand() % 0xFF
-			);
-			mlx_put_pixel(image, i, y, color);
-			y++;
-		}
-		i++;
-	}
-}
-
-int	main(void)
-{
-	void	*mlx;
-		
-	if (!(mlx = mlx_init(WIDTH, HEIGHT, "fractal starter", false)))
-	{
-		puts(mlx_strerror(mlx_errno));
-		return (EXIT_FAILURE);
-	}
-	if (!(image = mlx_new_image(mlx, 128, 128)))
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
-		return (EXIT_FAILURE);
-	}
-	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-	{
-		mlx_close_window(mlx);
-		puts(mlx_strerror(mlx_errno));
+		ft_printf("incorrect argument(s)\n\ttype \"Mandelbrot\""
+		"\n     or\ttype \"Julia\", value 1 and value 2\n");
 		return (EXIT_FAILURE);
 	}
 	
-	mlx_loop_hook(mlx, ft_randomize, mlx);
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	z.real = 0;
+	z.i = 0;
+	c.real = 0.25;
+	c.i = 0.4;
 	
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
+	// General function for Mandelbrot z^2 + c
+	for (int i = 0; i < 42; ++i)
+	{
+		// z^2
+		tmp_real = (z.real * z.real) - (z.i * z.i);
+		z.i = 2 * z.real * z.i;
+		z.real = tmp_real;
+		// + c
+		z.real = z.real + c.real;
+		z.i = z.i + c.i;
+		printf("iteration %d, real: %f, imaginary %f\n", i, z.real, z.i);
+	}
 	return (EXIT_SUCCESS);
 }
 
-
+// Calculate the math yourself -> z^2 + c -> almost // DONE
+// typedef struct for s_complex -> real and imaginary with doubles // DONE
+// Start up checks. // DONE
+// Put image onto window // DONE
+// key hook to print pressed key // DONE
 // test case for MLX42, not working...
 // setup MLX42 correctly, MLX42.h and make and make clean // DONE
 // Have window on screen // DONE
--- include/fractal.h
+++ include/fractal.h
@@ -6,22 +6,34 @@
 /*   By: tbruha <tbruha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 17:17:58 by tbruha            #+#    #+#             */
-/*   Updated: 2025/02/03 14:13:36 by tbruha           ###   ########.fr       */
+/*   Updated: 2025/02/04 11:50:42 by tbruha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTAL_H
 # define FRACTAL_H
 
+# define WIDTH 1920
+# define HEIGHT 1080
+
 // HEADERS
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
+# include <unistd.h>
+# include <math.h>
 # include "MLX42/MLX42.h"
 # include "MLX42/MLX42_Int.h"
+# include "libft.h"
 
 // STRUCTS
+
+typedef struct s_complex
+{
+	double	real;
+	double	i;
+} t_complex;
 
 // FTS
 
